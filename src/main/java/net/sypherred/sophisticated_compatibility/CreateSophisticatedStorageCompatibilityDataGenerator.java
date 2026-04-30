716bb9b0c361bf0293431c31082e076ad7d10810
SypherRed <ext.sypher@gmail.com>
2026-05-01 01:53:23 +0200

    Initial Upload

---
+++ src/main/java/net/sypherred/sophisticated_compatibility/CreateSophisticatedStorageCompatibilityDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sypherred.sophisticated_compatibility;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateSophisticatedStorageCompatibilityDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
